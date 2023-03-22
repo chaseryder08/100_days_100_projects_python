de936c237c85354714620d68a0ce41de98328138
Chase DeCoster <chasedecoster@gmail.com>
2023-03-22 12:43:13 -0400

    update push

--- Snake_project/scoreboard.py
+++ Snake_project/scoreboard.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 24, "normal")    
 
 
 class Scoreboard(Turtle):
@@ -8,7 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -21,7 +23,12 @@ class Scoreboard(Turtle):
 
     def reset(self):
         if self.score > self.high_score:
-            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.score}")
+            
+            with open("data.txt") as data:
+                self.high_score = int(data.read())
+            #self.high_score = self.score
         self.score = 0
         self.update_scoreboard()
             
